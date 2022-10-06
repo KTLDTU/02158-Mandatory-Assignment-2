843ee0d9a66087dde357156fae4c298696aa1d6f
Benjamin Braüner <s204461@student.dtu.dk>
2022-10-06 16:14:12 +0200

    Update Field.java

--- man2/Field.java
+++ man2/Field.java
@@ -5,19 +5,29 @@
 //Hans Henrik Lovengreen     Sep 26, 2022
 
 public class Field {
+    final int ROWS = 11;
+    final int COLS = 11;
+
+    Semaphore[][] S;
 
     public Field() {
+        S = new Semaphore[ROWS][COLS];
 
+        for (int row = 0; row < ROWS; row++) {
+            for (int col = 0; col < COLS; col++) {
+                S[row][col] = new Semaphore(1);
+            }
+        }
     }
 
     /* Block until car no. may safely enter tile at pos */
     public void enter(int no, Pos pos) throws InterruptedException {
-
+        S[pos.row][pos.col].P();
     }
 
     /* Release tile at position pos */
     public void leave(Pos pos) {
-
+        S[pos.row][pos.col].V();
     }
 
 }
