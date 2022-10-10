56006806a9eb563e1233a6e45be5c36c8a52ce65
KTLDTU <72263629+KTLDTU@users.noreply.github.com>
2022-10-10 15:01:54 +0200

    Implement promela model with semaphores

--- man2/SafeAlley.java
+++ man2/SafeAlley.java
@@ -5,27 +5,49 @@
 //Hans Henrik Lovengreen     Sep 26, 2022
 
 public class SafeAlley extends Alley {
+    int up, down;
+    Semaphore upSem, downSem, inDownSem, inUpSem;
 
     protected SafeAlley() {
-        
+        up = 0;   down = 0;
+        upSem   = new Semaphore(1);
+        downSem = new Semaphore(1);
+        inDownSem = new Semaphore(1);
+        inUpSem = new Semaphore(1);
     }
 
     /* Block until car no. may enter alley */
     public void enter(int no) throws InterruptedException {
         if (no < 5) {
-            
+            downSem.P();
+            inDownSem.P();
+            if (down == 0) upSem.P();    // block for up-going cars
+            down++;
+            inDownSem.V();
+            downSem.V();
         } else {
-            
+            upSem.P();
+            inUpSem.P();
+            if (up == 0) downSem.P();    // block for down-going cars
+            up++;
+            inUpSem.V();
+            upSem.V();
         }
 
-     }
+    }
 
     /* Register that car no. has left the alley */
-    public void leave(int no) {
+    public void leave(int no) throws InterruptedException {
         if (no < 5) {
-
+            inDownSem.P();
+            down--;
+            if (down == 0) upSem.V();    // enable up-going cars again
+            inDownSem.V();
         } else {
-
+            inUpSem.P();
+            up--;
+            if (up == 0) downSem.V();    // enable down-going cars again
+            inUpSem.V();
         }
     }
 
