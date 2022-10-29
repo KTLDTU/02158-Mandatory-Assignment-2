7873de4a8b87c734dc4929b0858126f10d079883
Benjamin <s204461@student.dtu.dk>
2022-10-29 17:41:38 +0200

    Remove Thread.sleep

--- man2/SafeAlley.java
+++ man2/SafeAlley.java
@@ -24,7 +24,6 @@ public class SafeAlley extends Alley {
 
                 if (turn == -1) {
                     turnSem.P();
-                    Thread.sleep(1000);
 
                     if (turn == -1) {
                         turn = 0;
